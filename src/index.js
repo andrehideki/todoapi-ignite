bc491471cef71ee5e0ecd9eb5e96ad2ae966381e
andrehideki <andre.hkiw.dev@gmail.com>
2022-05-13 12:10:26 -0300

    add: create user route

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const { v4: uuidV4 } = require('uuid');
+const { users } = require('./users');
+
 const app = express();
 const port = 3333;
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+    console.log(req.body)
+    const { name, username } = req.body;
+    const user = {
+        id: uuidV4(),
+        name,
+        username,
+        todos: []
+    }
+    users.push(user);
+    return res.status(201).send(user);
+});
 
 app.listen(port, () => console.log(`Application running at: ${port}`));
